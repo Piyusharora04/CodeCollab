96f94e4e3b828baaab41f9d3c4e7493560b5954d
Piyusharora04 <arorapiyush991@gmail.com>
2025-08-01 14:45:31 +0530

    backend-updated the db string

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const connectToMongoDb = (url) => {
     mongoose.connect(url , {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
     })
     .then(() => console.log("Database Connected"))
     .catch((err) => console.log("Error connecting to DB ",err))
