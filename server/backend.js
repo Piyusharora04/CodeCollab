1651860899b47cefb6a4eb2075107b1524659b8a
Piyusharora04 <arorapiyush991@gmail.com>
2025-08-01 10:40:40 +0530

    db string changed

--- server/backend.js
+++ server/backend.js
@@ -4,7 +4,7 @@ const { connectToMongoDb } = require("./config/connection.js");
 
 function setupBackend(app) {
 
-    connectToMongoDb("mongodb://db:27017/CodeCollab");
+    connectToMongoDb("mongodb+srv://PiyushArora:123456789p@cluster0.ncc8dv6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
     app.use("/user", userRouter);
 }
